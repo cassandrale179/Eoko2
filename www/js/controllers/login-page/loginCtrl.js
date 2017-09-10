7efb91a06b7bf70f1cb031ea6e080496b1fbaacc
Bao Thien Pham <baotpham7@gmail.com>
2017-09-10 01:11:35 -0400

    direct path to action page

--- www/js/controllers/login-page/loginCtrl.js
+++ www/js/controllers/login-page/loginCtrl.js
@@ -1,20 +1,25 @@
-app.controller('loginCtrl', function($scope, $firebaseObject) {
+app.controller('loginCtrl', function($scope, $state, $firebaseObject) {
   console.log("login page!");
-  
-    var ref = firebase.database().ref();
-  
-    var obj = $firebaseObject(ref);
-  
-    // For three-way data bindings, bind it to the scope instead
-    obj.$bindTo($scope, "data");
-  
-  
-    // firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(
-    //   function(resolve){
-    //     console.log("Logged in");
-    //     var usr = firebase.auth().currentUser;
-    //   });
+
+  $scope.$on('event:social-sign-in-success', function(event, userDetails){
+    var userUID = userDetails.uid;
+    console.log("User UID" + userDetails.uid);
+    console.log(userDetails);
+    console.log(userDetails.friends);
+
+    $state.go('tabsController.action')
+  });
+
+
 });
+
+
+
+// firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(
+//   function(resolve){
+//     console.log("Logged in");
+//     var usr = firebase.auth().currentUser;
+//   });
 
 
 /*
@@ -24,7 +29,7 @@ var obj = $firebaseObject(ref);
 
 // For three-way data bindings, bind it to the scope instead
 obj.$bindTo($scope, "data");
-*/ 
+*/
 
 //Old version
 // app.controller('loginCtrl', ['$scope', '$stateParams', '$state', 'UserInfo', '$ionicPopup',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
