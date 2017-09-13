b24bda1b5c0292831b4ab7f6667ce72eb4e20e44
Minh Le <lenguyetminh1998@gmail.com>
2017-09-13 12:09:36 -0400

    New Login Screen

--- www/controllers/loginCtrl.js
+++ www/controllers/loginCtrl.js
@@ -1,40 +1,4 @@
 app.controller('loginCtrl', function($scope, $firebaseObject) {
   
-    // window.fbAsyncInit = function() {
-    //         FB.init({
-    //         appId: '694354544087073',
-    //         status: true,
-    //         cookie: true,
-    //         xfbml: true
-    //     });
-    //     $scope.login = function() {
-    //         FB.login(function(response) {
-    //
-    //         // handle the response
-    //         console.log("Response goes here!");
-    //
-    //         }, {scope: 'read_stream,publish_stream,publish_actions,read_friendlists'});
-    //     }
-    //
-    //     $scope.logout = function() {
-    //         FB.logout(function(response) {
-    //           // user is now logged out
-    //         });
-    //     }
-    //
-    //     var status = FB.getLoginStatus();
-    //
-    //     console.log(status);
-    // };
-    //
-    // // Load the SDK asynchronously
-    // (function(d){
-    // var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    // if (d.getElementById(id)) {return;}
-    // js = d.createElement('script'); js.id = id; js.async = true;
-    // js.src = "//connect.facebook.net/en_US/all.js";
-    // ref.parentNode.insertBefore(js, ref);
-    // }(document));
-
 
 });
