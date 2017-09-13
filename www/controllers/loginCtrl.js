b82249366af579c293969fc731b3fe6eca10e902
Minh Le <lenguyetminh1998@gmail.com>
2017-09-13 13:02:59 -0400

    Initial Action Screen Front End

--- www/controllers/loginCtrl.js
+++ www/controllers/loginCtrl.js
@@ -1,4 +1,4 @@
-app.controller('loginCtrl', function($scope, $firebaseObject) {
+app.controller('loginCtrl', function($scope, $firebaseObject, $state) {
   $scope.$on('event:social-sign-in-success', function(event, userDetails){
     console.log(userDetails);
     $state.go('profile');
