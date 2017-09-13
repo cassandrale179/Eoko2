a73a3de83872ca244f02593c291e428a9b29db4f
Minh Le <lenguyetminh1998@gmail.com>
2017-09-13 15:33:31 -0400

    Backend for profile

--- www/controllers/profileCtrl.js
+++ www/controllers/profileCtrl.js
@@ -4,17 +4,13 @@ app.controller('profileCtrl', ['$scope', '$state','$firebaseArray',
 
     // ------------ THIS ALLOW USER TO MOVE BETWEEN TWO DIFFERENT SCREENS ON CREATE ACTION PAGE  --------
     $scope.description = 0;
-    $scope.action={};
+    $scope.action={}; 
 
 
 
     // ------------ WHEN USER CLICK SUBMIT, THIS FUNCTION WILL HAPPEN --------
     $scope.submit = function(){
-      console.log($scope.action);
+      var eventRef = firebase.database().ref("activities");
+      eventRef.push($scope.action);
     }
-
-
-
-
-
   }])
