fe6101e12bd9928a862625558ea8fa40ea047adc
Minh Le <lenguyetminh1998@gmail.com>
2017-09-14 12:38:40 -0400

    Backbone for action List page

--- www/controllers/actionList.js
+++ www/controllers/actionList.js
@@ -2,29 +2,30 @@ app.controller('actionListCtrl', ['$scope', '$state','$firebaseArray',
 
   function ($scope, $state, $firebaseArray) {
 
-    /* ----------------- FACEBOOK FRIENDS WHO ACTUALLY USE THE APP --------------------- */
-    // FB.api('/me/friends?limit=5000', function(res){
-    //   if(!res || res.error){
-    //     console.log('Error occured while fetching user details.');
-    //   }
-    //   else{
-    //     console.log(res);
-    //   }
-    // })
-
-
-    var currentUser = firebase.auth().currentUser;
-    $scope.friends =  []
-    // var friendsRef = firebase.database().ref("friends/" + currentUser.uid);
-    var friendsRef = firebase.database().ref("friends/0X1d3eosD8MyaUyvd8B5e92fiVo2")
-    friendsRef.once("value", function(snapshot){
-      var friendsTable  = snapshot.val().data;
-      for (var i = 0; i < friendsTable.length; i++){
-        $scope.friends.push(friendsTable[i]);
-      }
-      console.log($scope.friends)
+    //GET THE CURRENT USER WHO ARE USING THE APP
+    var currentUser;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        currentUser = user;
+    }
+    console.log(currentUser.uid);
+      getFriends();
     });
 
+
+    //GET THE CURRENT USER'S FRIEND LIST
+    function getFriends()
+    {
+      $scope.friends =  []
+      var friendsRef = firebase.database().ref("users/" + currentUser.uid + "/friendsinapp");
+      friendsRef.once("value", function(snapshot){
+        var friendsTable  = snapshot.val().data;
+        console.log(friendsTable);
+        for (var i = 0; i < friendsTable.length; i++){
+          $scope.friends.push(friendsTable[i]);
+        }
+      });
+    }
 
 
 
