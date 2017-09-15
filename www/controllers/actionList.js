d7ae4f7ddde1622eb71c303d438b0c94dc135469
sciencefreak500 <gkm24@drexel.edu>
2017-09-15 18:37:55 -0400

    location is now sorted by closest

--- www/controllers/actionList.js
+++ www/controllers/actionList.js
@@ -4,50 +4,91 @@ app.controller('actionListCtrl', ['$scope', '$state','$firebaseArray', '$http', 
 
     //GET THE CURRENT USER WHO ARE USING THE APP
     var currentUser;
-    $scope.nudge = 0
+    $scope.nudge = 0;
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         currentUser = user;
     }
     console.log(currentUser.uid);
+    ionicPlatform();
       getFriends();
-      ionicPlatform();
+      //ionicPlatform();
     });
 
 
     //GET THE CURRENT USER'S FRIEND LIST
     function getFriends()
     {
-      $scope.friends =  []
-      $scope.photos = []
+      $scope.friends =  [];
+      $scope.photos = [];
       var friendsRef = firebase.database().ref("users/");
-      friendsRef.on("value", function(snapshot){
 
-        //GET CURRENT USER'S INFORMATION (NAME AND PHOTO)
-        var userInfo = snapshot.child(currentUser.uid).val();
-        $scope.userPictureURL = userInfo.photoURL;
-        $scope.userName = userInfo.name;
-
-        //GET USER FRIEND'S INFORMATION UNDER THE USER'S SNAPSHOT
-        var friendsTable  = snapshot.child(currentUser.uid+"/friendsinapp").val().data;
-        console.log(friendsTable);
-
-        //CREATING A USER OBJECT FOR THE FRIENDS
-        for (var i = 0; i < friendsTable.length; i++){
-          $scope.friends.push(friendsTable[i]);
-
-          //FIND USER IN THE TABLE WHO HAS FB ID SIMILAR TO CURRENT USER'S FRIEND FBID
-          friendsRef.orderByChild("fbid").equalTo(friendsTable[i].id).on("child_added", function(snap) {
-            friendsTable[i].photo = snap.val().photoURL;
-            friendsTable[i].location = snap.val().location;
-            friendsTable[i].distance = snap.val().distance;
-            friendsTable[i].uid = snap.key;
+      $scope.friends = $firebaseArray(friendsRef);
+      $scope.friends.$loaded().then(function(x) {
+        console.log("gotlist", $scope.friends);
           });
-        }
-      });
+
     }
 
 
+    //------------------distance calculation--------------------
+
+    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {   
+            var R = 6371; // Radius of the earth in km
+            var dLat = deg2rad(lat2 - lat1); // deg2rad below
+            var dLon = deg2rad(lon2 - lon1);
+            var a =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+
+        function deg2rad(deg) {
+            return deg * (Math.PI / 180);
+        }
+
+        $scope.distFromPlayer = function(locationdata) {      
+
+          if($scope.myloc == undefined || $scope.myloc == null)
+          {
+            console.log("not yet");
+            return 0;
+          }
+          else
+          {
+            var arr = locationdata.split(",");
+            var lat = arr[0];
+            var long = arr[1];
+
+            var arr2 = $scope.myloc.split(",");
+            var mylat = arr2[0];
+            var mylong = arr2[1];
+
+            if (long == "" || lat == "" || mylong == "" || mylat == "") {
+                return "N/A";
+            } else {
+                var result = getDistanceFromLatLonInKm(mylat, mylong, lat, long) * 0.621371;
+                return Math.round(result * 10) / 10;
+            }
+          } 
+        };
+
+        $scope.distSorter = function(x)
+        {
+          if($scope.myloc == undefined || $scope.myloc == null)
+          {
+            return 0;
+          }
+          else
+          {
+            var result = $scope.distFromPlayer(x.location);
+            console.log("got it, its: ", result);
+            return result;
+          }
+        };
 
 
     //--------------------- REVERSE GEO-ENCODING ------------------------------
@@ -59,11 +100,16 @@ app.controller('actionListCtrl', ['$scope', '$state','$firebaseArray', '$http', 
       });
     }
 
-    //--------------------- ALWAYS WATCHING USER MOVEMENT AND PUSH IT TO FIREBASE ------
-    function ionicPlatform(){
-      $ionicPlatform.ready(function(){
+    //--------------------- ALWAYS WATCHING USER MOVEMENT AND PUSH IT TO FIREBASE ------\
+
+
+    function ionicPlatform()  //get geolocation data for current user
+    {
+      $ionicPlatform.ready(function()
+      {
         var watchId = navigator.geolocation.watchPosition(onSuccess);
-        function onSuccess(position) {
+        function onSuccess(position) 
+        {
           var latlng = position.coords.latitude + "," + position.coords.longitude;
           console.log("Latlng under ionic platform: " + latlng);
 
@@ -71,41 +117,17 @@ app.controller('actionListCtrl', ['$scope', '$state','$firebaseArray', '$http', 
           var userRef = firebase.database().ref("users/" + currentUser.uid);
           var obj = {
             location: latlng
-          }
+          };
           userRef.update(obj);
 
 
 
-          //------- GET USER'S CURRENT LOCATION ---------------
-          var lat1 = position.coords.latitude;
-          var lon1 = position.coords.longitude;
-
-
-          //-------- LOOP THROUGH THE FRIEND'S TABLE AND GET THEIR LOCATION -------
-          var earthRadius = 6371;
-          console.log("Calculating friend distance");
-          for (var i = 0; i < $scope.friends.length; i++){
-            var arr = $scope.friends[i].location.split(",");
-            var lat2 = arr[0];
-            var lon2 = arr[1];
-            var dLat = deg2rad(lat2-lat1);  // deg2rad below
-            var dLon = deg2rad(lon2-lon1);
-            var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-            var d = earthRadius * c; // Distance in km
-            var miles = d * 0.621371;
-            console.log($scope.friends[i].uid + ": " + miles);
-          }
+          $scope.myloc = latlng;  //actual current location
         }
       });
 
       }
 
-      function deg2rad(deg) {
-        return deg * (Math.PI/180);
-      }
 
 
-
-  }])
+  }]);
