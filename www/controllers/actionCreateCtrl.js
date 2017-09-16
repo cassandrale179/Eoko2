580fb3c5e805f0f2483097b619b4ee8ba2207d23
lenguyentri1999 <michaelle7312@gmail.com>
2017-09-16 16:08:35 -0400

    Finished authentication system, working on action create page

--- www/controllers/actionCreateCtrl.js
+++ www/controllers/actionCreateCtrl.js
@@ -1,12 +1,96 @@
-app.controller('actionCreateCtrl', ['$scope', '$state','$firebaseArray', '$ionicPlatform', '$http',
-  function ($scope, $state, $firebaseArray, $ionicPlatform, $http) {
+app.controller('actionCreateCtrl', ['$scope', '$state','$firebaseArray', '$ionicPlatform', '$http', '$window', 'ngFB',
+  function ($scope, $state, $firebaseArray, $ionicPlatform, $http, $window, ngFB) {
 
     //------ CHECK IF USER IS CURRENTLY LOGGING IN ------
     var currentUser;
+
     firebase.auth().onAuthStateChanged(function(user) {
       if (user){
-        console.log("user is logged in." + user);
+        console.log("user is logged in." + user.uid);
         currentUser = user;
+        var ref = firebase.database().ref('users/'+user.uid);
+
+        //Get user info
+        //TODO: put this in action List page
+        openFB.getLoginStatus(function(response){
+          if (response.status=="connected"){
+            console.log("response",  response.status);
+            openFB.api({
+              path: '/me',
+              params: {fields: 'id, name, gender, picture, birthday, friends'},
+            success: function(res){
+              console.log("Success!");
+              var userInfo = {
+                fbid: res.id,
+                name: res.name,
+                gender: res.gender,
+                photoURL: res.picture.data.url,
+                birthday: res.birthday
+              }
+
+              //Get firebase ID of friends who are in app
+              var ref = firebase.database().ref("users");
+              var friendsList = {};
+
+              userFriendsRef = firebase.database().ref('users/'+user.uid+"/friends");
+              angular.forEach(res.friends.data, function(friend){
+                var friendID = friend.id;
+                console.log(friendID);
+                ref.orderByChild('fbid').equalTo(friendID).on("child_added", function(snapshot){
+                  var obj = {};
+                  obj[snapshot.key] = snapshot.val().name;
+
+                  userFriendsRef.update(obj);
+                })
+
+              });
+
+
+              //Update all info to Firebase
+              console.log("user info");
+              console.log('friends list', friendsList);
+              console.log(userInfo);
+              userRef = firebase.database().ref('users/'+user.uid);
+
+              userRef.update(userInfo);
+              console.log("friends list below");
+              console.log(friendsList);
+              // userFriendsRef.update(friendsList);
+
+
+
+
+            },
+            error: function(error){
+              console.log("Error while using open FB");
+              console.log(error);
+            }});
+          }
+        });
+
+
+
+        // console.log(facebookService.getUserInfo());
+
+
+        // openFB.getLoginStatus(function(response){
+        //   if (response.status=="connected"){
+        //     console.log("response",  response.status);
+        //     openFB.api({
+        //       path: '/me',
+        //       params: {fields: 'id, name,email, friends'},
+        //     success: function(res){
+        //       console.log("Success!");
+        //       console.log(res.email);
+        //     },
+        //     error: function(error){
+        //       console.log("Error while using open FB");
+        //       console.log(error);
+        //     }});
+        //   }
+        // })
+
+        // $scope.getUserInfo();
       }
       else{
         console.log("No user")
@@ -14,7 +98,7 @@ app.controller('actionCreateCtrl', ['$scope', '$state','$firebaseArray', '$ionic
     })
 
 
-    //------------------- GET USER CURRENT LOCATION -------------------------------------- 
+    //------------------- GET USER CURRENT LOCATION --------------------------------------
     $ionicPlatform.ready(function(){
       var watchId = navigator.geolocation.watchPosition(onSuccess);
       function onSuccess(position) {
@@ -40,6 +124,9 @@ app.controller('actionCreateCtrl', ['$scope', '$state','$firebaseArray', '$ionic
 
     // ------------ WHEN USER CLICK SUBMIT, THIS FUNCTION WILL HAPPEN --------
     $scope.submit = function(){
+      var ref = firebase.database().ref('activities');
       console.log($scope.action);
     }
+
+
   }])
