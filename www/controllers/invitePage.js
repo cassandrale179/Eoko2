008ec067de80fd2a51fa9595ecb5d1436d2643b1
Minh Le <lenguyetminh1998@gmail.com>
2017-10-02 22:10:09 -0400

    Add in invites only tag

--- www/controllers/invitePage.js
+++ www/controllers/invitePage.js
@@ -1,16 +1,20 @@
 app.controller('invitePageCtrl', ['$scope', '$state', '$firebaseAuth',
 function($scope, $state, $firebaseAuth){
+
+  //----------------- LIST OF ARRAYS TO BE USED BY THIS CONTROLLER --------------
   $scope.userFriendObject = []
   $scope.splitArr = []
+  $scope.invitedPeople = []
   var newArr = []
 
-  //start the thing in case it starts here
+
+  //------------------- GET CURRENT USER UID -------------------
   firebase.auth().onAuthStateChanged(function(user){
     if (user){
       $scope.currentUser = user;
     }
 
-    //---------- EXTRACT FRIEND LIST OF THE AUTHOR -----------
+    //------------------- EXTRACT FRIEND LIST OF THE AUTHOR -----------------
     var friendsRef = firebase.database().ref("users/" + $scope.currentUser.uid + "/friends");
     friendsRef.on("value", function(snapshot){
       var friendTable = snapshot.val();
@@ -22,14 +26,35 @@ function($scope, $state, $firebaseAuth){
       console.log($scope.userFriendObject);
 
 
-      //------ SPLIT ARRAY ---------
+      //---------------------- SPLIT ARRAY INTO CHUNKS -----------------------
       chunk = 3
       for (i=0,j=$scope.userFriendObject.length; i<j; i+=chunk) {
           temparray = $scope.userFriendObject.slice(i,i+chunk);
           $scope.splitArr.push(temparray);
       }
-
       console.log($scope.splitArr);
+
+
+      //---------------------- WHEN USER CLICK INVITE FRIENDS  -----------------------
+      $scope.inviteFriend = function(id){
+        var className = document.getElementById(id).className;
+        if (className == "opaque activated"){
+          document.getElementById(id).className = "not-opaque activated";
+          $scope.invitedPeople.push(id);
+        }
+        if (className == "not-opaque activated"){
+          document.getElementById(id).className = "opaque activated";
+          toDeleteIndex = $scope.invitedPeople.indexOf(id);
+          $scope.invitedPeople.splice(toDeleteIndex,1);
+        }
+      }
+
+
+      //---------------------- WHEN USER SUBMIT AN INVITATION -----------------------
+      $scope.invitedSubmit = function(){
+        console.log($scope.invitedPeople);
+      }
+
 
     })
 
